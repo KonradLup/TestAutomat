25ca6031431a9bd350803dafb1db4682e49614e9
KonradLup <konrad.lupinski@gmial.com>
2023-01-22 13:28:21 +0100

    First project created

---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UltimateQATest {
+    String projectLocation = System.getProperty("user.dir");
+    WebDriver driver;
+
+
+    //hermetyzacja: public/private/protected
+    @Test
+    public void testone() {
+        System.out.println(projectLocation);
+        System.setProperty("webdriver.chrome.driver", projectLocation + "/resources/chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+
+        driver.close();
+
+
+    }
+
+}
